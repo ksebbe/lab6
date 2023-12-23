cbe799ac23ac5091a73310ba9eca98a730dc20d8
Kseniya Blagireva <ksenia062003@gmail.com>
2023-12-23 12:02:37 +0300

    first5

--- tasks/src/tasks/tasks.controller.ts
+++ tasks/src/tasks/tasks.controller.ts
@@ -24,7 +24,7 @@ export class TasksController {
 
 
   @Post(':id/assign')
-  ..async assignTask(@Param('id') id: string, @Body('userId') userId: string) {
+  async assignTask(@Param('id') id: string, @Body('userId') userId: string) {
     return this.tasksService.assignTask(id, userId);
   }
 
