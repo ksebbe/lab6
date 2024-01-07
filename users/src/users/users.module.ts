1ff5f4b5a463f960cd326e3cfafc2368bcddfc3d
Masha <mtikhonova18@gmail.com>
2024-01-07 19:26:12 +0300

    6

--- users/src/users/users.module.ts
+++ users/src/users/users.module.ts
@@ -4,6 +4,7 @@ import { UsersController } from './users.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserSchema } from './entity/users.schema';
 
+
 @Module({
   imports: [
     MongooseModule.forFeature([
